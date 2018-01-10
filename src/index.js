b2765219a8dadb0f5e66e48e2055dbde245ab5b4
zhenghui0705 <648338777qq.com>
2018-01-10 11:45:49 +0800

    修复setMath函数bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function setMath(){
     let sum = 0
     for(let i = 0;i < arguments.length; i++)
@@ -6,13 +5,3 @@ function setMath(){
     return sum
 }
 console.log(setMath(1,2,3));
-=======
-function setSum (){
-    var sum = 0;
-    for(var i = 0;i < arguments.length; i++)
-    sum += arguments[i]
-    return sum
-};
-
-console.log(setSum(10,22,33));
->>>>>>> dev
